10b0ae0af742a716cefeba8ee565300a418b2213
Gurkirat Singh <gurkiratsingh2001@gmail.com>
2022-01-06 14:45:24 +0530

    changing paths to my system

--- rpvio_estimator/scripts/path_commander.py
+++ rpvio_estimator/scripts/path_commander.py
@@ -22,18 +22,24 @@ inited = False
 client = airsim.MultirotorClient(ip='10.2.36.227')
 client.confirmConnection()
 client.enableApiControl(True)
-client.takeoffAsync()
-client.hoverAsync()
+client.armDisarm(True)
+client.takeoffAsync().join()
+client.landAsync().join()
+print("Done 1")
 
-#client.moveToZAsync(-2, 0.5)
-#client.moveToZAsync(0, 0.5)
-#client.moveToZAsync(-2.5, 0.5)
+client.moveToZAsync(-3,.5).join()
+client.moveToZAsync(-1,.5).join()
+client.moveToZAsync(-3,.5).join()
 
 path = []
 path.append(airsim.Vector3r(0.0, 0.0, -5))
 path.append(airsim.Vector3r(0.0, 0.0, -1))
 path.append(airsim.Vector3r(0.0, 0.0, -5))
 client.moveOnPathAsync(path, 2.25, np.inf, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(True, 0.5)).join()
+#path.append(airsim.Vector3r(0.0, 0.0, -3))
+#client.moveOnPathAsync(path, 0.5, np.inf, airsim.DrivetrainType.ForwardOnly).join()
+#print("Done")
+#print("Done 2")
 
 prev_goal = None
 
