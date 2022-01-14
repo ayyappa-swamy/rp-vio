f4145913bf4118a1d2c270bf96fa580a4ed9fb7f
Gurkirat Singh <gurkiratsingh2001@gmail.com>
2022-01-14 20:13:55 +0530

    ros node v0.5

--- plane_segmentation/plane_segmentation/planercnn/PlaneSegmentor.py
+++ plane_segmentation/plane_segmentation/planercnn/PlaneSegmentor.py
@@ -1,14 +1,29 @@
 import numpy as np
 from detect import Detector
 
+
 #
 class PlaneSegmentor:
-    def __init__(self, options, config, camera):
+    def __init__(self, options, config, camera, ids_file='seg_rgbs.txt',
+                 ground_id=212, sky_id=24):
         self.camera = camera
         self.config = config
         self.options = options
         self.detector = Detector(options, config, camera)
-        pass
+        self.mapping = {}
+        self.rev_mapping = -1 * np.ones((256, 256, 256), dtype=int)
+        self.save_ids(ids_file)
+        self.ground_id = ground_id
+        self.sky_id = sky_id
+
+    def save_ids(self, ids_file: str):
+        with open(ids_file, 'r') as f:
+            for line in f.readlines():
+                _id, color = tuple(line[:-1].split('\t'))
+                _id = int(_id)
+                color = np.fromstring(color[1:-1], dtype=np.uint8, sep=',')
+                self.mapping[_id] = color
+                self.rev_mapping[color[0], color[1], color[2]] = _id
 
     def segment(self, rgb_image, seg_image) -> np.ndarray:
         """
@@ -19,7 +34,11 @@ class PlaneSegmentor:
 
         Returns: plane segmented image
         """
-        pass
+        masks, parameters = self.detect_planes(rgb_image)
+        masks = self.filter_planes(rgb_image, seg_image, masks)
+        plane_mask = self.merge_masks(masks)
+
+        return plane_mask
 
     def detect_planes(self, rgb_image) -> np.ndarray:
         """
@@ -37,10 +56,23 @@ class PlaneSegmentor:
 
         Returns: filtered masks
         """
-        pass
+        id_image = self.rev_mapping[
+            building_seg[:, :, 0], building_seg[:, :, 1], building_seg[:, :, 2]]
+        sky_mask = id_image == self.sky_id
+        ground_mask = id_image == self.ground_id
+
+        for mask in masks:
+            mask[sky_mask | ground_mask] = 0
+
+        return masks
 
     def merge_masks(self, masks) -> np.ndarray:
         """
         Merge n masks into a segmented image
         """
-        pass
+        plane_seg = np.zeros((*masks.shape[1:], 3))
+
+        for i, mask in masks:
+            plane_seg[mask == 1] = self.mapping[i + 1]
+
+        return plane_seg
--- plane_segmentation/plane_segmentation/planercnn/ros_node.py
+++ plane_segmentation/plane_segmentation/planercnn/ros_node.py
@@ -1,12 +1,36 @@
 from options import parse_args
 from config import InferenceConfig
-import pickle as pkl
+import numpy as np
+from PlaneSegmentor import PlaneSegmentor
+import rospy
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+
+class Node:
+    def __init__(self, options, config, camera, image_topic='/image',
+                 mask_publishing_topic='/plane_mask', rate=10):
+        rospy.init_node("Planercnn Node", anonymous=True)
+        self.segmentor = PlaneSegmentor(options, config, camera)
+        self.sub = rospy.Subscriber(image_topic, Image, self.callback)
+        self.bridge = CvBridge()
+        self.pub = rospy.Publisher(mask_publishing_topic, Image, queue_size=10)
+        self.image = None
+        self.rate = rospy.Rate(rate)
+
+    def callback(self, message):
+        self.image = self.bridge.imgmsg_to_cv2(message)
+
+    def start(self):
+        rospy.loginfo("Starting segmentation")
+        rospy.spin()
+        while not rospy.is_shutdown():
+            rospy.loginfo('Running')
+            self.pub.publish(self.bridge.cv2_to_imgmsg(self.image))
+            self.rate.sleep()
+
 
 def main():
-    pass
-
-
-if __name__ == '__main__':
     args = parse_args()
 
     if args.dataset == '':
@@ -24,6 +48,11 @@ if __name__ == '__main__':
         args.printInfo = True
         pass
     options = args
-    with open('options.pkl', 'wb') as f:
-        pkl.dump(options,f)
     config = InferenceConfig(options)
+    camera = np.array([320, 320, 320, 240, 640, 480])
+    node = Node(options, config, camera)
+    node.start()
+
+
+if __name__ == '__main__':
+    main()
